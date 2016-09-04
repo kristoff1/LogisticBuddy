ead1f1673e7e3725b1d1837d20354bba6769c70c
nisie <nathania.sutedja@tokopedia.com>
2016-09-04 21:44:31 +0700

    Add Service

--- app/src/main/java/com/nisnis/batp/logisticbuddy/MenuActivity.java
+++ app/src/main/java/com/nisnis/batp/logisticbuddy/MenuActivity.java
@@ -43,6 +43,7 @@ public class MenuActivity extends AppCompatActivity {
         initializeFirebase();
         initializeMenu(this);
 
+        getApplicationContext().startService(new Intent(this, DriveCompanionService.class));
     }
 
     private void initializeMenu(Context context) {
